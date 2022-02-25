ff3349a81286e0bfa40fcd89d115f50fe7202b4b
deepanshu363 <goyaldeepanshu363@gmail.com>
2022-02-25 11:58:28 +0530

    added post request with request schemea

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
 from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Blog(BaseModel):
+    title: str
+    Name: "str"
 
 
 @app.get("/")
@@ -17,3 +23,8 @@ def root():
 @app.get("/query/{idi}")
 def root(message: str, idi, name: Optional[str] = None):
     return {"message": f'"{message} just fetching the request {idi}"'}
+
+
+@app.post("/base")
+def root(requests: Blog):
+    return {"message": f'"{requests.Name} deepansu"'}
