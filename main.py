f08e4b22d8fda8220ea31917295e6ad6716f8c50
deepanshu363 <goyaldeepanshu363@gmail.com>
2022-02-25 11:31:10 +0530

    added parametric get request

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
 
@@ -6,3 +7,13 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message": "Hello World"}
+
+
+@app.get("/fetch")
+def root():
+    return {"message": "just fetching the request"}
+
+
+@app.get("/query/{idi}")
+def root(message: str, idi, name: Optional[str] = None):
+    return {"message": f'"{message} just fetching the request {idi}"'}
