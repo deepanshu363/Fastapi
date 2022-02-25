00142c397d8cd9ed53ff246506284320b364364e
deepanshu363 <goyaldeepanshu363@gmail.com>
2022-02-25 19:01:21 +0530

    Implementing sql database using sqlalchemy which is used in pthon

--- main.py
+++ main.py
@@ -1,35 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from typing import Optional
-from pydantic import BaseModel
+
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
+import models
+import schemas
 
 app = FastAPI()
+models.Base.metadata.create_all(engine)
 
 
-class Blog(BaseModel):
-    title: str
-    Name: "str"
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
-@app.get("/")
-def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/fetch")
-def root():
-    return {"message": "just fetching the request"}
-
-
-@app.get("/query/{idi}")
-def root(message: str, idi, name: Optional[str] = None):
-    return {"message": f'"{message} just fetching the request {idi}"'}
-
-
-@app.post("/base")
-def root(requests: Blog):
-    return {"message": f'"{requests.Name} deepanshu"'}
-
-
-@app.post("/base2")
-def root(requests: Blog):
-    return {"message": f'"{requests.Name} deepanshu"'}
+@app.post("/dbreq")
+def root(requests: schemas.Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=requests.title, body=requests.Name)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
