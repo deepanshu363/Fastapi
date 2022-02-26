f600361dec0f0a1d06332c3f4d27ee1bfab41fd7
deepanshu363 <goyaldeepanshu363@gmail.com>
2022-02-26 16:38:40 +0530

    all requests updated get,post,put,delete

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Response, status
 from typing import Optional
 
 from database import engine, SessionLocal
@@ -19,15 +19,48 @@ def get_db():
 
 
 @app.post("/dbreq")
-def root(requests: schemas.Blog, db: Session = Depends(get_db)):
-    new_blog = models.Blog(title=requests.title, body=requests.Name)
+def root(tablename: str, requests: schemas.Blog, db: Session = Depends(get_db)):
+    if tablename == "Table1":
+        new_blog = models.Table1(title=requests.title, body=requests.Name)
+    elif tablename == "Table2":
+        new_blog = models.Table2(title=requests.title, body=requests.Name)
+    else:
+        return "NO SUCH TABLE EXIST AT ALL...."
+
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
     return new_blog
 
 
-@app.get("/all")
-def root(db: Session = Depends(get_db)):
-    blogs = db.query(models.Blog).filter(models.Blog.id == ).first()
+@app.get("/table1")
+def root(ids: int, response: Response, db: Session = Depends(get_db)):
+    blogs = db.query(models.Table1).filter(models.Table1.id == ids).first()
+    if not blogs:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return "NOT FOUND!!!😩"
     return blogs
+
+
+@app.get("/table2")
+def root(ids: int, response: Response, db: Session = Depends(get_db)):
+    blogs = db.query(models.Table2).filter(models.Table2.id == ids).first()
+    if not blogs:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return "NOT FOUND!!!😩"
+    return blogs
+
+
+@app.put("/update")
+def root(ids: int, response: Response, requests: schemas.Blog, db: Session = Depends(get_db)):
+    db.query(models.Table1).filter(models.Table1.id == ids).update({"title": requests.title, "body": requests.Name})
+
+    db.commit()
+    return "updates"
+
+
+@app.delete("/delete")
+def root(ids: int, response: Response, db: Session = Depends(get_db)):
+    db.query(models.Table1).filter(models.Table1.id == ids).delete()
+    db.commit()
+    return "Delete"
--- models.py
+++ models.py
@@ -3,10 +3,15 @@ from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
 from database import Base
 
 
-class Blog(Base):
+class Table1(Base):
     __tablename__ = 'blogs'
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
     body = Column(String)
 
 
+class Table2(Base):
+    __tablename__ = 'blos'
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    body = Column(String)
